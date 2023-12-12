1066100dd829696166d3e4ce1c749deb642a3c2b
Zykov Anton <anton.zykov1@gmail.com>
2023-12-13 00:39:25 +0100

    Add solution for Notification

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,44 @@
 export default class NotificationMessage {
+  element = null;
 
+  constructor(message, { duration, type } = {}) {
+    this.message = message;
+    this.duration = duration ?? 1000;
+    this.type = type ?? 'success';
+
+    this.element = this.createElement();
+  }
+
+  createTemplate() {
+    return `
+      <div class="notification ${this.type}" style="--value:${this.duration}ms">
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+          <div class="notification-header">Header</div>
+          <div class="notification-body">
+            ${this.message}
+          </div>
+        </div>
+      </div>
+    `;
+  }
+
+  createElement() {
+    const element = document.createElement('div');
+    element.innerHTML = this.createTemplate();
+    return element.firstElementChild;
+  }
+
+  show(container = document.body) {
+    container.append(this.element);
+    setTimeout(() => this.remove(), this.duration);
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+  }
 }
