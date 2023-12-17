41dab91142a1028a38d0e3c3997dbcf3bc667b20
Zykov Anton <anton.zykov1@gmail.com>
2023-12-17 23:04:46 +0100

    Revert "Add solution for Sortable Table v2"
    This reverts commit 15464e7cb5f2de3be4ab7a06fbadbb25d993fc4e.

--- 06-events-practice/1-sortable-table-v2/index.js
+++ 06-events-practice/1-sortable-table-v2/index.js
@@ -1,47 +1,8 @@
-import SortableTablev1 from '../../05-dom-document-loading/2-sortable-table-v1/index.js';
-
-export default class SortableTable extends SortableTablev1 {
+export default class SortableTable {
   constructor(headersConfig, {
     data = [],
     sorted = {}
   } = {}) {
-    super(headersConfig, data);
-    this.isSortLocally = true;
-    this.sorted = { ...sorted };
-    this.sort();
-    this.createListenersForHeaders();
-  }
 
-  createListenersForHeaders() {
-    for (let headerCell of this.subElements.header.children) {
-      if (headerCell.dataset.sortable === 'true') {
-        headerCell.addEventListener('pointerdown', () => {
-          this.sorted.id = headerCell.dataset.id;
-          this.sorted.order = headerCell.dataset.order === 'desc' ? 'asc' : 'desc';
-          this.sort();
-        });
-      }
-    }
-  }
-
-  sort() {
-    if (this.isSortLocally) {
-      this.sortOnClient();
-    } else {
-      this.sortOnServer();
-    }
-  }
-
-  sortOnClient() {
-    super.sort(this.sorted.id, this.sorted.order);
-  }
-
-  sortOnServer() {
-    return;
-  }
-
-  filter(field, value) {
-    this.data = this.data.filter((item) => item[field] === value);
-    this.sort();
   }
 }
